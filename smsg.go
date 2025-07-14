34d66caba946ea2768254e026143eef349e76e7c
Nils O. Selåsdal <nos@utel.tech>
2025-07-14 20:37:34 +0200

    go fmt

--- smsg.go
+++ smsg.go
@@ -17,7 +17,7 @@ const gVariableLen = -2
 
 var gHex = [...]byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
 
-//fast implementation
+// fast implementation
 func uint16ToHex(v uint16) []byte {
 	var b []byte
 	return append(b, gHex[(v&0xf000)>>12], gHex[(v&0xf00)>>8], gHex[(v&0x00f0)>>4], gHex[(v&0x000f)])
@@ -36,24 +36,24 @@ func (s *RawSMsg) addImpl(tag uint16, length int, data []byte) {
 	s.Data = append(s.Data, data...)
 }
 
-//Add adds a new tag with the given data
-//Note, newlines (\r or \n) must not occur within the data
+// Add adds a new tag with the given data
+// Note, newlines (\r or \n) must not occur within the data
 func (s *RawSMsg) Add(tag uint16, data []byte) {
 	s.addImpl(tag & ^gConstructor, len(data), data)
 }
 
-//AddVariableTag adds a new tag with a variable lenght tag
+// AddVariableTag adds a new tag with a variable lenght tag
 func (s *RawSMsg) AddVariableTag(tag uint16) {
 	s.addImpl(tag|gConstructor, gVariableLen, []byte{})
 }
 
-//AddRaw adds the entire content of r as the value of a new tag
+// AddRaw adds the entire content of r as the value of a new tag
 func (s *RawSMsg) AddRaw(tag uint16, r *RawSMsg) {
 	s.addImpl(tag|gConstructor, len(r.Data), r.Data)
 }
 
-//AddSafe is a safe replacement for Add where newlines within data (\r or \n)
-//is escaped.
+// AddSafe is a safe replacement for Add where newlines within data (\r or \n)
+// is escaped.
 func (s *RawSMsg) AddSafe(tag uint16, data []byte) {
 	r := make([]byte, 0, len(data))
 	for _, c := range data {
@@ -71,12 +71,12 @@ func (s *RawSMsg) AddSafe(tag uint16, data []byte) {
 	s.addImpl(tag, len(r), r)
 }
 
-//An Iter used to iterate through Tags
+// An Iter used to iterate through Tags
 type Iter struct {
 	data []byte
 }
 
-//A Tag of an SMsg
+// A Tag of an SMsg
 type Tag struct {
 	Tag         uint16
 	Constructor bool
@@ -88,17 +88,18 @@ func (t *Tag) String() string {
 	return fmt.Sprintf("Tag: 0x%04X C:%t Data:%s", t.Tag, t.Constructor, t.Data)
 }
 
-//Tags returns an iterator used to iterate all the tags in the SMsg
+// Tags returns an iterator used to iterate all the tags in the SMsg
 func (s *RawSMsg) Tags() Iter {
 	return Iter{s.Data}
 }
 
+// SubTags iterate over tags stored in the data of the current tag
 func (t *Tag) SubTags() Iter {
 	return Iter{t.Data}
 }
 
-//NextTag returns the next Tag in the SMsg or an error.
-//io.EOF is returned when there is no more tags to iterate
+// NextTag returns the next Tag in the SMsg or an error.
+// io.EOF is returned when there is no more tags to iterate
 func (i *Iter) NextTag() (t Tag, err error) {
 	if len(i.data) < 4 { //tag
 		return t, io.EOF
@@ -150,15 +151,15 @@ func (i *Iter) NextTag() (t Tag, err error) {
 	return t, nil
 }
 
-//RawSMsgReader is used to read RawSMsgs from a stream.
+// RawSMsgReader is used to read RawSMsgs from a stream.
 type RawSMsgReader struct {
 	//reader to read SMsgs from
 	R         *bufio.Reader
 	lastError error
 }
 
-//NewRawSMsgReader returns a new RawSMsgReader reading from r.
-//r is wrapped in a *bufio.Reader unless it already is a *bufio.Reader
+// NewRawSMsgReader returns a new RawSMsgReader reading from r.
+// r is wrapped in a *bufio.Reader unless it already is a *bufio.Reader
 func NewRawSMsgReader(r io.Reader) RawSMsgReader {
 	rr := RawSMsgReader{}
 	if bufR, ok := r.(*bufio.Reader); ok {
@@ -169,10 +170,10 @@ func NewRawSMsgReader(r io.Reader) RawSMsgReader {
 	return rr
 }
 
-//ReadRawSMsg returns the next RawSmsg or an error.
-//error will be io.EOF when the end is reached
-//The returned RawSmsg could be empty if an empty line
-//is encountered.
+// ReadRawSMsg returns the next RawSmsg or an error.
+// error will be io.EOF when the end is reached
+// The returned RawSmsg could be empty if an empty line
+// is encountered.
 func (r *RawSMsgReader) ReadRawSMsg() (RawSMsg, error) {
 	l, err := r.R.ReadBytes('\n')
 	if r.lastError != nil {
